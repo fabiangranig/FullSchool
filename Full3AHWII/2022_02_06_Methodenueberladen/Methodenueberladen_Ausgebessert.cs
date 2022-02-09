23128ec8e27d1a2c97620a20e6e49b96f3f1da7b
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-02-09 09:48:48 +0100

    Pyramiden und Methodenueberladen_Ausgebessert hinzugefügt.

---
+++ Full3AHWII/2022_02_06_Methodenueberladen/Methodenueberladen_Ausgebessert.cs
@@ -0,0 +1,179 @@
+﻿//Fabian Granig 3AHWII
+//06.02.2022
+//Beispiel: Punkt2D /  Methoden überladen
+using System;
+
+namespace _20220206_Punkt2D
+{
+    class Punkt2D
+    {
+        //Variables for the class
+        public double X;
+        public double Y;
+
+        //Standart Konstruktor1
+        public Punkt2D()
+        {
+            this.X = 0;
+            this.Y = 0;
+        }
+
+        //X, Y Konstruktor
+        public Punkt2D(double X1, double Y1)
+        {
+            this.X = X1;
+            this.Y = Y1;
+        }
+
+        //X Konstruktor
+        public Punkt2D(double X1)
+        {
+            this.X = X1;
+            this.Y = X1;
+        }
+
+        //function to move the inputed data to the Punkt2D
+        public void PunktEinlesen(double x, double y)
+        {
+            //Put the values in the Punkt2D struct
+            this.X = x;
+            this.Y = y;
+        }
+
+        //function to output the data into the console
+        public void PunktAusgeben()
+        {
+            //Output with CW
+            Console.WriteLine("Die X-Kordinate beträgt: {0}", this.X);
+            Console.WriteLine("Die Y-Kordinate beträgt: {0}", this.Y);
+        }
+
+        //function to get the distance between two point
+        public double Laenge(Punkt2D punkt2)
+        {
+            //Calculate with the formular
+            double laenge = Math.Sqrt(Math.Pow(this.X - punkt2.X, 2) + Math.Pow(this.Y - punkt2.Y, 2));
+
+            //Return the value
+            return laenge;
+        }
+
+        //function to get the mid of two point
+        public Punkt2D GeradeMittelpunkt(Punkt2D punkt2)
+        {
+            //Declare a temp Punkt2D
+            Punkt2D punkt2D_temp = new Punkt2D();
+
+            //Calculate the mid point with the formular
+            punkt2D_temp.X = (this.X + punkt2.X) / 2;
+            punkt2D_temp.Y = (this.Y + punkt2.Y) / 2;
+
+            //Return the value
+            return punkt2D_temp;
+        }
+
+        //function to get the "Steigung"
+        public double Steigung(Punkt2D punkt2)
+        {
+            //Calculate "Steigung"
+            double steigung = (punkt2.Y - this.Y) / (punkt2.X - this.X);
+
+            //return the solution
+            return steigung;
+        }
+
+        //function Mul with point
+        public double Mul(Punkt2D punkt2)
+        {
+            //Calculation
+            double solution = this.X * punkt2.Y + this.Y * punkt2.X;
+
+            //Return the punkt
+            return solution;
+        }
+
+        //function Mul with lenth
+        public double Mul(double length1)
+        {
+            //Calculation
+            double solution = this.X * length1 + this.Y * length1;
+
+            //Return the solution
+            return solution;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input the data1
+            Console.WriteLine("Punkt1 Eingabe: ");
+            Console.Write("Geben Sie bitte die X-Cordinate ein: ");
+            double X_Cord1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Geben Sie bitte die Y-Cordinate ein: ");
+            double Y_Cord1 = Convert.ToDouble(Console.ReadLine());
+
+            //empty Line
+            Console.WriteLine(" ");
+
+            //Input the data2
+            Console.WriteLine("Punkt2 Eingabe: ");
+            Console.Write("Geben Sie bitte die X-Cordinate ein: ");
+            double X_Cord2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Geben Sie bitte die Y-Cordinate ein: ");
+            double Y_Cord2 = Convert.ToDouble(Console.ReadLine());
+
+            //Apply function "PunktEinlesen"
+            Punkt2D punkt1 = new Punkt2D(X_Cord1, Y_Cord1);
+            Punkt2D punkt2 = new Punkt2D(X_Cord2, Y_Cord2);
+
+            //empty Line
+            Console.WriteLine(" ");
+
+            //Give back the cordinates with function "PunktAusgeben"
+            Console.WriteLine("Punkt1 Ausgabe:");
+            punkt1.PunktAusgeben();
+
+            //empty Line
+            Console.WriteLine(" ");
+
+            //Give back the second cordinates with function "PunktAusgeben"
+            Console.WriteLine("Punkt2 Ausgabe: ");
+            punkt2.PunktAusgeben();
+
+            //empty Line
+            Console.WriteLine(" ");
+
+            //get the "Laenge" of the two points with the function "Laenge"
+            double laenge = punkt1.Laenge(punkt2);
+            Console.WriteLine("Die Länge beträgt: {0}", laenge);
+
+            //get the mid point of those two points
+            Punkt2D mid_point = punkt1.GeradeMittelpunkt(punkt2);
+            Console.WriteLine("Der Mittelpunkt ist der Punkt ({0} / {1})", mid_point.X, mid_point.Y);
+
+            //get the "Steigung"
+            double steigung = punkt1.Steigung(punkt2);
+
+            Console.WriteLine("Die Steigung beträgt: {0}", steigung);
+
+            //empty Line
+            Console.WriteLine("");
+
+            //Mul point * point
+            double er1 = punkt1.Mul(punkt2);
+            Console.WriteLine("Die zwei Punkte multiplizieren: " + er1);
+
+            //empty Line
+            Console.WriteLine("");
+
+            //Mul point * length
+            Console.WriteLine("Punkt 1 multipliziert mit einer Länge:");
+            Console.Write("Bitte die Länge eingeben: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            double er2 = punkt1.Mul(length);
+            Console.WriteLine("Ergebnis: " + er2);
+        }
+    }
+}
---
+++ Full3AHWII/2022_02_09_Pyramide/Pyramiden.cs
@@ -0,0 +1,203 @@
+﻿//Fabian Granig 3AHWII
+//09.02-2022
+//Beispiel: Pyramide
+using System;
+
+namespace _20220209_Pyramide
+{
+    class Pyramide
+    {
+        //Variablen der Klasse
+        public double laenge;
+        public double breite;
+        public double hoehe;
+
+        //Standartkonstruktor
+        public Pyramide()
+        {
+            this.laenge = 0;
+            this.breite = 0;
+            this.hoehe = 0;
+        }
+
+        //einen Wert Konstruktor
+        public Pyramide(double eingabe)
+        {
+            this.laenge = eingabe;
+            this.breite = eingabe;
+            this.hoehe = eingabe;
+        }
+
+        //zwei Wert Konstruktor
+        public Pyramide(double seite1, double hoehe1)
+        {
+            this.laenge = seite1;
+            this.breite = seite1;
+            this.hoehe = hoehe1;
+        }
+
+        //drei Werte Konstruktor
+        public Pyramide(double laenge1, double breite1, double hoehe1)
+        {
+            this.laenge = laenge1;
+            this.breite = breite1;
+            this.hoehe = hoehe1;
+        }
+
+        //Methode Ändern
+        public void Ändern(double laenge1, double breite1, double hoehe1)
+        {
+            this.laenge = laenge1;
+            this.breite = breite1;
+            this.hoehe = hoehe1;
+        }
+
+        //Methode Anzeigen
+        public void Anzeigen()
+        {
+            Console.WriteLine("Die Länge beträgt: " + this.laenge);
+            Console.WriteLine("Die Breite beträgt: " + this.breite);
+            Console.WriteLine("Die Höhe beträgt: " + this.hoehe);
+        }
+
+        //Methode Volumen
+        public double Volumen()
+        {
+            //Berechnung mit Formel
+            double ergebnis = (this.laenge * this.breite) * this.hoehe / 3;
+
+            //Das Ergebnis zurückgeben
+            return ergebnis;
+        }
+
+        //Methode Oberfläche
+        public double Oberfläche()
+        {
+            //Berechnung von den zwei Seitenkanten
+            double sa = Math.Sqrt(Math.Pow(this.hoehe, 2) + Math.Pow((this.laenge / 2), 2));
+            double sb = Math.Sqrt(Math.Pow(this.hoehe, 2) + Math.Pow((this.breite / 2), 2));
+
+            //Berechnung der Seiten Oberfläche
+            double a_flaeche = this.laenge * sa;
+            double b_flaeche = this.breite * sb;
+
+            //Berechnung der Bodenfläche
+            double bodenflaeche = this.laenge * this.breite;
+
+            //Berechnung der Formel
+            double ergebnis = a_flaeche + b_flaeche + bodenflaeche;
+
+            //Das Ergebnis zurückgeben
+            return ergebnis;
+        }
+
+        //Methode Vergleichen
+        public int Vergleichen(Pyramide py2)
+        {
+            //Von beiden die Fläche berechnnen
+            double py1_flaeche = Oberfläche();
+            double py2_flaeche = py2.Oberfläche();
+
+            //Checken welche Fläche die größte ist
+            if(py1_flaeche > py2_flaeche)
+            {
+                return 1;
+            }
+            else
+            {
+                if(py2_flaeche > py1_flaeche)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        //Methode IstQuadratische
+        public bool IstQuadratische()
+        {
+            if(this.laenge == this.breite && this.breite == this.hoehe)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array mit 6 Pyramiden
+            Pyramide[] pyramides = new Pyramide[6];
+
+            //Einlesen der Seiten der Pyramide
+            for(int i = 0; i < pyramides.Length; i++)
+            {
+                //Eingabe
+                Console.Write("Geben Sie bitte die Länge der {0}.Pyramide ein: ", i+1);
+                double laenge = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Geben Sie bitte die Breite der {0}.Pyramide ein: ", i + 1);
+                double breite = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Geben Sie bitte die Hoehe der {0}.Pyramide ein: ", i + 1);
+                double hoehe = Convert.ToDouble(Console.ReadLine());
+
+                //Bauen mit Konstruktor
+                pyramides[i] = new Pyramide(laenge, breite, hoehe);
+            }
+
+            //leere Zeile
+            Console.WriteLine();
+
+            //Kontrollieren wieviele davon Quadratisch sind
+            int zaehler = 0;
+            for(int i = 0; i < pyramides.Length; i++)
+            {
+                if(pyramides[i].IstQuadratische())
+                {
+                    zaehler++;
+                }
+            }
+
+            //Ausgabe der Quadratischen Pyramiden
+            Console.WriteLine("Es sind {0} Pyramiden quadratisch.", zaehler);
+
+            //Gesamtes Volumen aller Pyaramiden
+            double volumen = 0;
+            for(int i = 0; i < pyramides.Length; i++)
+            {
+                volumen += pyramides[i].Volumen();
+            }
+
+            //Ausgabe des gesamten Volumens
+            Console.WriteLine("Das Volumen aller Pyramiden beträgt: " + volumen);
+
+            //Welche Pyramide besitzt die größte Fläche
+            Pyramide highest = pyramides[0];
+            int zaehler2 = 0;
+            for(int i = 1; i < pyramides.Length; i++)
+            {
+                //Vergleichen
+                if(highest.Vergleichen(pyramides[i]) == -1)
+                {
+                    highest = pyramides[i];
+                    zaehler2 = i;
+                }
+            }
+
+            //leere Zeile
+            Console.WriteLine();
+
+            //Die größte Pyramide ausgeben
+            int zaehler3 = zaehler2 + 1;
+            Console.WriteLine("Die größte Pyramide ist " + zaehler3 + " mit den Werten: ");
+            pyramides[zaehler2].Anzeigen();
+        }
+    }
+}
