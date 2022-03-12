95ce0b74d68323c3caf3c6fe6244a4fb2e8e5489
fabiang <fabiangranig.fg@gmail.com>
2022-03-12 04:54:23 -0500

    Removed tabstops.

--- Full3AHWII/2022_03_12_Kontoverwaltungssystem/Kontoverwaltungssystem.cs
+++ Full3AHWII/2022_03_12_Kontoverwaltungssystem/Kontoverwaltungssystem.cs
@@ -2,52 +2,52 @@
 
 namespace Template_Datei
 {
-		//Klasse Konto
-		class Konto
-		{		
-				//Erstellen von Variablen
-				private int kontonummer;
-				private double kontostand;
-				private int anzahl_buchungen;
+	//Klasse Konto
+	class Konto
+	{		
+		//Erstellen von Variablen
+		private int kontonummer;
+		private double kontostand;
+		private int anzahl_buchungen;
 
-				//Kapselung
-				public Kontonummer
-				{
-						get { return kontonummer; }
-				}
-				public Kontostand
-				{
-						get { return kontostand; }
-						set { kontostand = Kontostand; }
-				}
-				public Anzahl_buchungen
-				{
-						get { return anzahl_buchungen; }
-						set { anzahl_buchungen = Anzahl_buchungen; }
-				}
-
-				//Konstruktor
-				public Konto(int kontonummer1)
-				{
-						this.kontonummer = kontonummer1;
-						this.kontostand = 0;
-						this.anzahlbuchungen = 0;
-				}
-				public Konto(int kontonummer1, double kontostand1, int anzahl_buchungen1)
-				{
-						this.kontonummer = kontonummer1;
-						this.kontostand = kontostand1;
-						this.anzahlbuchungen = anzahl_buchungen1;
-				}
+		//Kapselung
+		public Kontonummer
+		{
+			get { return kontonummer; }
+		}
+		public Kontostand
+		{
+			get { return kontostand; }
+			set { kontostand = Kontostand; }
+		}
+		public Anzahl_buchungen
+		{
+			get { return anzahl_buchungen; }	
+			set { anzahl_buchungen = Anzahl_buchungen; }
 		}
 
-		class Program
-    {
-        static void Main(string[] args)
-        {
-						//Erstellen von zwei Konton
-						Konto konto1 = new Konto(5432);
-						Konto konto2 = new Konto(2345, 100, 1);
-        }
-    }
+		//Konstruktor
+		public Konto(int kontonummer1)
+		{
+			this.kontonummer = kontonummer1;	
+			this.kontostand = 0;			
+			this.anzahlbuchungen = 0;
+		}
+		public Konto(int kontonummer1, double kontostand1, int anzahl_buchungen1)
+		{
+			this.kontonummer = kontonummer1;
+			this.kontostand = kontostand1;
+			this.anzahlbuchungen = anzahl_buchungen1;
+		}
+	}
+
+	class Program
+    	{
+        	static void Main(string[] args)
+        	{
+			//Erstellen von zwei Konton
+			Konto konto1 = new Konto(5432);
+			Konto konto2 = new Konto(2345, 100, 1);
+		}
+    	}
 }
