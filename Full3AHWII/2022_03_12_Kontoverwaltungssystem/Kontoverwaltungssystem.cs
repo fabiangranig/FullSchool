e9930e0ac094ad88836682c8476d580dc1dc9900
fabiang <fabiangranig.fg@gmail.com>
2022-03-12 05:11:31 -0500

    Full3AHWII: Kontoverwaltungssystem finished

--- Full3AHWII/2022_03_12_Kontoverwaltungssystem/Kontoverwaltungssystem.cs
+++ Full3AHWII/2022_03_12_Kontoverwaltungssystem/Kontoverwaltungssystem.cs
@@ -11,19 +11,17 @@ namespace Template_Datei
 		private int anzahl_buchungen;
 
 		//Kapselung
-		public Kontonummer
+		public int Kontonummer
 		{
 			get { return kontonummer; }
 		}
-		public Kontostand
+		public double Kontostand
 		{
 			get { return kontostand; }
-			set { kontostand = Kontostand; }
 		}
-		public Anzahl_buchungen
+		public int Anzahl_buchungen
 		{
 			get { return anzahl_buchungen; }	
-			set { anzahl_buchungen = Anzahl_buchungen; }
 		}
 
 		//Konstruktor
@@ -31,13 +29,26 @@ namespace Template_Datei
 		{
 			this.kontonummer = kontonummer1;	
 			this.kontostand = 0;			
-			this.anzahlbuchungen = 0;
+			this.anzahl_buchungen = 0;
 		}
 		public Konto(int kontonummer1, double kontostand1, int anzahl_buchungen1)
 		{
 			this.kontonummer = kontonummer1;
 			this.kontostand = kontostand1;
-			this.anzahlbuchungen = anzahl_buchungen1;
+			this.anzahl_buchungen = anzahl_buchungen1;
+		}
+
+		//Methoden
+		public void Auf_Ab_Buchen(double betrag)
+		{
+			this.kontostand += betrag;
+			this.anzahl_buchungen++;
+		}
+		public void Anzeigen()
+		{
+			Console.WriteLine("Die Kontonummer ist: " + this.kontonummer);
+			Console.WriteLine("Der Kontostand beträgt: " + this.kontostand);
+			Console.WriteLine("Die Anzahl der Buchungen beträgt: "+ this.anzahl_buchungen);
 		}
 	}
 
@@ -48,6 +59,27 @@ namespace Template_Datei
 			//Erstellen von zwei Konton
 			Konto konto1 = new Konto(5432);
 			Konto konto2 = new Konto(2345, 100, 1);
+
+			//Beide Konten anzeigen
+			Console.WriteLine("Konto1 wird angezeigt: ");
+			konto1.Anzeigen();
+			Console.WriteLine("");
+			Console.WriteLine("Konto2 wird angezeigt: ");
+			konto2.Anzeigen();
+
+			//leere Zeile
+			Console.WriteLine("----------------------------------------------");
+
+			//Bei beiden Konten was ab und auf buchen
+			konto1.Auf_Ab_Buchen(300);
+			konto2.Auf_Ab_Buchen(-50);
+
+			//Beide Konten anzeigen
+			Console.WriteLine("Konto1 wird angezeigt: ");
+			konto1.Anzeigen();
+			Console.WriteLine("");
+			Console.WriteLine("Konto2 wird angezeigt: ");
+			konto2.Anzeigen();
 		}
     	}
 }
