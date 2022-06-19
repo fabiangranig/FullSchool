8f7744e5d4e129d0603c64e64059b3599e4ba235
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-06-19 14:10:18 +0200

    Added the second version of the ListBoxWechsler with Insert

---
+++ Full3AHWII/2022_04_27_ListBoxWechsler_mit_Insert2/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace _20220427_ListBoxWechsler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lB1 = new System.Windows.Forms.ListBox();
+            this.lB2 = new System.Windows.Forms.ListBox();
+            this.btn_nachRechts = new System.Windows.Forms.Button();
+            this.btn_nachLinks = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_alldelete = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Laden = new System.Windows.Forms.Button();
+            this.btn_SaveBinaer = new System.Windows.Forms.Button();
+            this.btn_LoadBinaer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lB1
+            // 
+            this.lB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lB1.FormattingEnabled = true;
+            this.lB1.ItemHeight = 24;
+            this.lB1.Location = new System.Drawing.Point(22, 22);
+            this.lB1.Margin = new System.Windows.Forms.Padding(6);
+            this.lB1.Name = "lB1";
+            this.lB1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lB1.Size = new System.Drawing.Size(481, 628);
+            this.lB1.TabIndex = 0;
+            // 
+            // lB2
+            // 
+            this.lB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lB2.FormattingEnabled = true;
+            this.lB2.ItemHeight = 24;
+            this.lB2.Location = new System.Drawing.Point(961, 22);
+            this.lB2.Margin = new System.Windows.Forms.Padding(6);
+            this.lB2.Name = "lB2";
+            this.lB2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lB2.Size = new System.Drawing.Size(481, 628);
+            this.lB2.TabIndex = 1;
+            // 
+            // btn_nachRechts
+            // 
+            this.btn_nachRechts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nachRechts.Location = new System.Drawing.Point(517, 22);
+            this.btn_nachRechts.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_nachRechts.Name = "btn_nachRechts";
+            this.btn_nachRechts.Size = new System.Drawing.Size(433, 159);
+            this.btn_nachRechts.TabIndex = 2;
+            this.btn_nachRechts.Text = ">>";
+            this.btn_nachRechts.UseVisualStyleBackColor = true;
+            this.btn_nachRechts.Click += new System.EventHandler(this.btn_nachRechts_Click);
+            // 
+            // btn_nachLinks
+            // 
+            this.btn_nachLinks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nachLinks.Location = new System.Drawing.Point(517, 192);
+            this.btn_nachLinks.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_nachLinks.Name = "btn_nachLinks";
+            this.btn_nachLinks.Size = new System.Drawing.Size(433, 159);
+            this.btn_nachLinks.TabIndex = 3;
+            this.btn_nachLinks.Text = "<<";
+            this.btn_nachLinks.UseVisualStyleBackColor = true;
+            this.btn_nachLinks.Click += new System.EventHandler(this.btn_nachLinks_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.Location = new System.Drawing.Point(517, 362);
+            this.btn_Delete.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(433, 159);
+            this.btn_Delete.TabIndex = 4;
+            this.btn_Delete.Text = "Löschen";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Exit.Location = new System.Drawing.Point(1012, 665);
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(433, 159);
+            this.btn_Exit.TabIndex = 5;
+            this.btn_Exit.Text = "Beenden";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // btn_alldelete
+            // 
+            this.btn_alldelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_alldelete.Location = new System.Drawing.Point(517, 532);
+            this.btn_alldelete.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_alldelete.Name = "btn_alldelete";
+            this.btn_alldelete.Size = new System.Drawing.Size(433, 159);
+            this.btn_alldelete.TabIndex = 6;
+            this.btn_alldelete.Text = "Alles löschen";
+            this.btn_alldelete.UseVisualStyleBackColor = true;
+            this.btn_alldelete.Click += new System.EventHandler(this.btn_alldelete_Click);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(22, 665);
+            this.btn_Save.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(236, 78);
+            this.btn_Save.TabIndex = 7;
+            this.btn_Save.Text = "Speichern";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Laden
+            // 
+            this.btn_Laden.Location = new System.Drawing.Point(22, 746);
+            this.btn_Laden.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_Laden.Name = "btn_Laden";
+            this.btn_Laden.Size = new System.Drawing.Size(236, 78);
+            this.btn_Laden.TabIndex = 8;
+            this.btn_Laden.Text = "Laden";
+            this.btn_Laden.UseVisualStyleBackColor = true;
+            this.btn_Laden.Click += new System.EventHandler(this.btn_Laden_Click);
+            // 
+            // btn_SaveBinaer
+            // 
+            this.btn_SaveBinaer.Location = new System.Drawing.Point(267, 665);
+            this.btn_SaveBinaer.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_SaveBinaer.Name = "btn_SaveBinaer";
+            this.btn_SaveBinaer.Size = new System.Drawing.Size(236, 78);
+            this.btn_SaveBinaer.TabIndex = 9;
+            this.btn_SaveBinaer.Text = "Speichern Binär";
+            this.btn_SaveBinaer.UseVisualStyleBackColor = true;
+            this.btn_SaveBinaer.Click += new System.EventHandler(this.btn_SaveBinaer_Click);
+            // 
+            // btn_LoadBinaer
+            // 
+            this.btn_LoadBinaer.Location = new System.Drawing.Point(267, 746);
+            this.btn_LoadBinaer.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_LoadBinaer.Name = "btn_LoadBinaer";
+            this.btn_LoadBinaer.Size = new System.Drawing.Size(236, 78);
+            this.btn_LoadBinaer.TabIndex = 10;
+            this.btn_LoadBinaer.Text = "Laden Binär";
+            this.btn_LoadBinaer.UseVisualStyleBackColor = true;
+            this.btn_LoadBinaer.Click += new System.EventHandler(this.btn_LoadBinaer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1467, 831);
+            this.Controls.Add(this.btn_LoadBinaer);
+            this.Controls.Add(this.btn_SaveBinaer);
+            this.Controls.Add(this.btn_Laden);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.btn_alldelete);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_nachLinks);
+            this.Controls.Add(this.btn_nachRechts);
+            this.Controls.Add(this.lB2);
+            this.Controls.Add(this.lB1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "ListBox Wechsler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lB1;
+        private System.Windows.Forms.ListBox lB2;
+        private System.Windows.Forms.Button btn_nachRechts;
+        private System.Windows.Forms.Button btn_nachLinks;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_alldelete;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Laden;
+        private System.Windows.Forms.Button btn_SaveBinaer;
+        private System.Windows.Forms.Button btn_LoadBinaer;
+    }
+}
+
---
+++ Full3AHWII/2022_04_27_ListBoxWechsler_mit_Insert2/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _20220427_ListBoxWechsler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            //Close the programm
+            this.Close();
+        }
+
+        private void btn_nachRechts_Click(object sender, EventArgs e)
+        {
+            //Save the information from the listbox
+            int runs = lB1.SelectedIndices.Count;
+            int[] list1 = new Int32[runs];
+            int[] list2 = new Int32[runs];
+            for (int i = 0; i < runs; i++)
+            {
+                //Save index + number
+                int index = lB1.SelectedIndex;
+                list1[i] = index + i;
+                list2[i] = Int32.Parse(Convert.ToString(lB1.Items[index]));
+
+                //Delete the value
+                lB1.Items.RemoveAt(index);
+            }
+
+            //Remove the selected items
+            while (lB1.SelectedItems.Count > 0)
+            {
+                lB1.Items.Remove(lB1.SelectedItems[0]);
+            }
+
+            //Insert these values on the other site
+            for(int i = 0; i < list1.Length; i++)
+            {
+                int howlarge = lB2.Items.Count;
+                if(howlarge > list1[i] + i)
+                {
+                    lB2.Items.Insert(list1[i], list2[i]);
+                }
+                else
+                {
+                    lB2.Items.Add(list2[i]);
+                }
+            }
+        }
+
+        
+
+        private void btn_nachLinks_Click(object sender, EventArgs e)
+        {
+            //Save the information from the listbox
+            int runs = lB2.SelectedIndices.Count;
+            int[] list1 = new Int32[runs];
+            int[] list2 = new Int32[runs];
+            for (int i = 0; i < runs; i++)
+            {
+                //Save index + number
+                int index = lB2.SelectedIndex;
+                list1[i] = index + i;
+                list2[i] = Int32.Parse(Convert.ToString(lB2.Items[index]));
+
+                //Delete the value
+                lB2.Items.RemoveAt(index);
+            }
+
+            //Remove the selected items
+            while (lB2.SelectedItems.Count > 0)
+            {
+                lB2.Items.Remove(lB2.SelectedItems[0]);
+            }
+
+            //Insert these values on the other site
+            for (int i = 0; i < list1.Length; i++)
+            {
+                int howlarge = lB1.Items.Count;
+                if (howlarge > list1[i] + i)
+                {
+                    lB1.Items.Insert(list1[i], list2[i]);
+                }
+                else
+                {
+                    lB1.Items.Add(list2[i]);
+                }
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            //Remove the selected items for lB1
+            while (lB1.SelectedItems.Count > 0)
+            {
+                lB1.Items.Remove(lB1.SelectedItems[0]);
+            }
+
+            //Remove the selected items for lB2
+            while (lB2.SelectedItems.Count > 0)
+            {
+                lB2.Items.Remove(lB2.SelectedItems[0]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Create Random variables
+            Random random = new Random();
+
+            //for-Loop to get the values in the boxes
+            for (int i = 0; i < 10; i++)
+            {
+                lB1.Items.Add(random.Next(1, 20));
+            }
+        }
+
+        private void btn_alldelete_Click(object sender, EventArgs e)
+        {
+            //Alles aus den Listboxes entfernen
+            lB1.Items.Clear();
+            lB2.Items.Clear();
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            //Speichern der ersten Listbox
+            FileStream fs = new FileStream("text.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
+            for(int i = 0; i < lB1.Items.Count; i++)
+            {
+                sw.WriteLine(lB1.Items[i]);
+            }
+
+            sw.Close();
+            fs.Close();
+
+            //Speichern der zweiten Listbox
+            FileStream fs2 = new FileStream("text2.txt", FileMode.Create);
+            StreamWriter sw2 = new StreamWriter(fs2);
+
+            for (int i = 0; i < lB2.Items.Count; i++)
+            {
+                sw2.WriteLine(lB2.Items[i]);
+            }
+
+            sw2.Close();
+            fs2.Close();
+        }
+
+        private void btn_Laden_Click(object sender, EventArgs e)
+        {
+            //Laden der ersten ListBox
+            FileStream zeichen = new FileStream("text.txt", FileMode.Open);
+            StreamReader lesen = new StreamReader(zeichen);
+
+            lB1.Items.Clear();
+
+            string zeilen = lesen.ReadLine();
+            while (zeilen != null)
+            {
+                lB1.Items.Add(zeilen);
+                zeilen = lesen.ReadLine();
+            }
+
+            lesen.Close();
+
+            //Laden der zweite ListBox
+            FileStream zeichen2 = new FileStream("text2.txt", FileMode.Open);
+            StreamReader lesen2 = new StreamReader(zeichen2);
+
+            lB2.Items.Clear();
+
+            string zeilen2 = lesen2.ReadLine();
+            while (zeilen2 != null)
+            {
+                lB2.Items.Add(zeilen2);
+                zeilen2 = lesen2.ReadLine();
+            }
+
+            lesen2.Close();
+        }
+
+        private void btn_SaveBinaer_Click(object sender, EventArgs e)
+        {
+            //Speichern der ersten Listbox
+            FileStream fs = new FileStream("text.dat", FileMode.Create);
+            BinaryWriter sw = new BinaryWriter(fs);
+
+            for (int i = 0; i < lB1.Items.Count; i++)
+            {
+                string u = Convert.ToString(lB1.Items[i]);
+                sw.Write(u);
+            }
+
+            sw.Close();
+            fs.Close();
+
+            //Speichern der zweiten Listbox
+            FileStream fs2 = new FileStream("text2.dat", FileMode.Create);
+            BinaryWriter sw2 = new BinaryWriter(fs2);
+
+            for (int i = 0; i < lB2.Items.Count; i++)
+            {
+                string u = Convert.ToString(lB2.Items[i]);
+                sw2.Write(u);
+            }
+
+            sw2.Close();
+            fs2.Close();
+        }
+
+        private void btn_LoadBinaer_Click(object sender, EventArgs e)
+        {
+            //Laden der ersten ListBox
+            FileStream zeichen = new FileStream("text.dat", FileMode.Open);
+            BinaryReader lesen = new BinaryReader(zeichen);
+
+            lB1.Items.Clear();
+
+            while (lesen.PeekChar() > -1)
+            {
+                string z = lesen.ReadString();
+                lB1.Items.Add(z);
+            }
+
+            lesen.Close();
+
+            //Laden der zweite ListBox
+            FileStream zeichen2 = new FileStream("text2.dat", FileMode.Open);
+            BinaryReader lesen2 = new BinaryReader(zeichen2);
+
+            lB2.Items.Clear();
+
+            while (lesen2.PeekChar() > -1)
+            {
+                string z = lesen2.ReadString();
+                lB2.Items.Add(z);
+            }
+
+            lesen2.Close();
+        }
+    }
+}
