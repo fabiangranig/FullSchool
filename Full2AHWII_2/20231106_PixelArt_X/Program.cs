04374c03fdb90721322c65e77a4602a21dcf338c
Fabian <fabiangranig.fg@gmail.com>
2023-11-07 10:20:40 +0100

    Added Random Code Task of second class

---
+++ Full2AHWII_2/20231106_PixelArt_X/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _20231106_PixelArt_X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OnlyOneIf(20);
+        }
+
+        static void NoIf(int size)
+        {
+            
+        }
+
+        static void OnlyOneIf(int size)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    if (x == y || size - 1 - y == x)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("x");
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Full2AHWII_2/20231106_PixelArt_X/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("20231106_PixelArt_X")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("20231106_PixelArt_X")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("b0e2fe1d-ff6b-44d1-bf6a-b761cd9fc35c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
