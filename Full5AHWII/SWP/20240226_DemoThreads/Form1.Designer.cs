c16b9f92ec7b97b9619a364784229bbd9a51ea3a
unknown <fabianshrewd>
2024-02-26 11:19:17 +0100

    Added program demo threads

---
+++ Full5AHWII/SWP/20240226_DemoThreads/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _20240226_DemoThreads
+{
+    partial class DemoThreads
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Text1 = new System.Windows.Forms.Label();
+            this.button_Start1 = new System.Windows.Forms.Button();
+            this.button_thread2 = new System.Windows.Forms.Button();
+            this.label_Text2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_Text1
+            // 
+            this.label_Text1.AutoSize = true;
+            this.label_Text1.Location = new System.Drawing.Point(12, 9);
+            this.label_Text1.Name = "label_Text1";
+            this.label_Text1.Size = new System.Drawing.Size(0, 16);
+            this.label_Text1.TabIndex = 0;
+            // 
+            // button_Start1
+            // 
+            this.button_Start1.Location = new System.Drawing.Point(12, 415);
+            this.button_Start1.Name = "button_Start1";
+            this.button_Start1.Size = new System.Drawing.Size(102, 23);
+            this.button_Start1.TabIndex = 1;
+            this.button_Start1.Text = "StartThread";
+            this.button_Start1.UseVisualStyleBackColor = true;
+            this.button_Start1.Click += new System.EventHandler(this.button_Start1_Click);
+            // 
+            // button_thread2
+            // 
+            this.button_thread2.Location = new System.Drawing.Point(120, 415);
+            this.button_thread2.Name = "button_thread2";
+            this.button_thread2.Size = new System.Drawing.Size(102, 23);
+            this.button_thread2.TabIndex = 2;
+            this.button_thread2.Text = "Start Thread 2";
+            this.button_thread2.UseVisualStyleBackColor = true;
+            this.button_thread2.Click += new System.EventHandler(this.button_thread2_Click);
+            // 
+            // label_Text2
+            // 
+            this.label_Text2.AutoSize = true;
+            this.label_Text2.Location = new System.Drawing.Point(189, 9);
+            this.label_Text2.Name = "label_Text2";
+            this.label_Text2.Size = new System.Drawing.Size(0, 16);
+            this.label_Text2.TabIndex = 3;
+            // 
+            // DemoThreads
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_Text2);
+            this.Controls.Add(this.button_thread2);
+            this.Controls.Add(this.button_Start1);
+            this.Controls.Add(this.label_Text1);
+            this.Name = "DemoThreads";
+            this.Text = "DemoThreads";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DemoThreads_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Text1;
+        private System.Windows.Forms.Button button_Start1;
+        private System.Windows.Forms.Button button_thread2;
+        private System.Windows.Forms.Label label_Text2;
+    }
+}
+
---
+++ Full5AHWII/SWP/20240226_DemoThreads/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20240226_DemoThreads
+{
+    public partial class DemoThreads : Form
+    {
+        private Thread _thread1;
+        private Thread _thread2;
+
+        public DemoThreads()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateUI(string text, bool isFirst)
+        {
+            if(InvokeRequired)
+            {
+                BeginInvoke(new Action<string, bool>(UpdateUI), text, isFirst);
+            }
+            else
+            {
+                if(isFirst)
+                {
+                    this.label_Text1.Text += text + "\n";
+                }
+                else
+                {
+                    this.label_Text2.Text += text + "\n";
+                }
+            }
+        }
+
+        private void WorkerThread(object parameter)
+        {
+            bool isFirst = (bool)parameter;
+            for(int i = 0; i < 10; i++)
+            {
+                UpdateUI($"Schleifendurchlauf: {i + 1}", isFirst);
+                Thread.Sleep(1000);
+            }
+            UpdateUI("Thread beendet.", isFirst);
+        }
+
+        private void button_Start1_Click(object sender, EventArgs e)
+        {
+            button_Start1.Enabled = false;
+            _thread1 = new Thread(WorkerThread);
+            _thread1.Start(true);
+        }
+
+        private void DemoThreads_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(this._thread1 != null && this._thread1.IsAlive)
+            {
+                _thread1.Abort();
+            }
+            if (this._thread2 != null && this._thread2.IsAlive)
+            {
+                _thread2.Abort();
+            }
+        }
+
+        private void button_thread2_Click(object sender, EventArgs e)
+        {
+            button_thread2.Enabled = false;
+            _thread2 = new Thread(WorkerThread);
+            _thread2.Start(false);
+        }
+    }
+}
